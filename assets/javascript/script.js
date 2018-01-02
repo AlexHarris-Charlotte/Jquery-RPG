b414910aff7f18119ec7cc040976f75bceaa125c
Alex Harris <akharri8@ncsu.edu>
2018-01-02 10:24:07 -0500

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,8 +3,8 @@
 // can store characters in an array
 // Use jquery to append characters to a div on the page
 //  
-//  adf
-//  adf
+//  
+//  
 
 function character(name, image, health, attackPower, counterAttack) {
     this.name = name;
@@ -20,7 +20,51 @@ var grevious = new character("General Grevious", "https://media2.giphy.com/media
 var windu = new character("Mace Windu", "https://media0.giphy.com/media/3ornjTfcat9eNI1wg8/200.gif#2-grid1",200, 15, 25);
 
 var charArray = [obiWan, anakin, grevious, windu];
+var value;
+
+// Give each character a value of i + 1
 
 for(var i = 0; i < charArray.length; i++) {
-    $("")
+    var nameP = $("<p>");
+    var imgP = $("<p>");
+    var healthP = $("<p>");
+    var newImg = $("<img>");
+    nameP.attr("value", i);
+    nameP.text(charArray[i].name);
+    nameP.addClass("position centered character");
+    newImg.attr("src", charArray[i].image)
+    newImg.addClass("image");
+    healthP.text(charArray[i].health);
+    imgP.append(newImg);
+    nameP.append(imgP);
+    nameP.append(healthP);
+    $("#characters").append(nameP);
 }
+
+
+// on click need to keep selected character in the top div
+// maybe remove the clicked character from the charArray and use another forloop to append to enemies div
+// need to move the other characters to enemies available to attack
+$(".character").on("click", function() {
+    $(this).attr("id", "indexRemove");
+    $(".character").not(this).remove();
+    value = parseInt(value);
+    if(this.value === 0) {
+        console.log("works");
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
